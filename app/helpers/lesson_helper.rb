b7295326dcaddfc294518f9ca35485d22a1305e2
押元亮真 <buono3berryz7@gmail.com>
2019-09-25 09:14:58 +0800

    I modified

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -2,8 +2,4 @@ module LessonHelper
     def is_complete(category)
         Lesson.find_by(user_id: current_user.id,category_id: category.id)
     end
-
-    # def lesson(category)
-    #     Lesson.find_by(user_id: current_user.id,category_id: category.id)
-    # end
 end
